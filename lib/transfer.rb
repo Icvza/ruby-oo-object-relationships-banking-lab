58d8b9aea6e593bb4ab8f675a133f07a502b11da
Isaac <deisaacalejandro@gmail.com>
2020-12-09 06:27:15 -0500

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,20 +3,18 @@ require 'pry'
 
 class Transfer
   attr_accessor :sender, :status, :bank_account
+  attr_reader :receiver
 
-  def initialize(sender, status="pending", receiver)
+  def initialize(sender, status="pending", receiver, amount)
     @sender = sender
     @receiver = receiver
     @status = "pending"
+    @amount = amount
   end
 
   def receiver=(receiver)
     bidning.pry
     self. receiver = Bankaccount
-  end
-
-  def receiver
-    @receiver
   end
 
 
